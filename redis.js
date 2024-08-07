f115765e6fe60a85dbb541012d78fb5721c8c28b
Amaury Delannoy <delannoy.amaury.pro@gmail.com>
2024-08-07 15:41:10 +0200

    add fake dataset

--- redis.js
+++ redis.js
@@ -8,80 +8,247 @@ const client = redis.createClient({
     }
 });
 
+client.connect(); //TODO ADEL se renseigner sur multi() ?
 client.on('error', err => console.log('Redis Client Error', err));
 
-async function main() {
-    await client.connect(); //TODO ADEL se renseigner sur multi() ?
-
-    // Init data set
-    const messageKey1 = "idMessage1"
-    const message1 = {
-        id: messageKey1,
-        author: {
-            id: "idAAA",
-            name: "AAA"
-        },
-        channel: {
-            id: "channelId",
-            name: "echange-retours"
-        },
-        attachments: [],
-        date: "01-01-1991 02:10:00",
-        content: "Premier message en 'BDD' Redis"
-    }
-
-    const messageKey2 = "idMessage2"
-    const message2 = {
-        id: messageKey2,
-        author: {
-            id: "idZF",
-            name: "ZF"
-        },
-        channel: {
-            id: "channelId",
-            name: "echange-retours"
-        },
-        attachments: [],
-        date: "16-01-1991 00:00:00",
-        content: "Message de l'autre BG là"
-    }
-
-    const messages = [message1, message2]
-
-    // Clear datas
-    client.del(messageKey1)
-    client.del(messageKey2)
-    client.del("messages")
-
-    // Store Map
-    saveRedisObject(messageKey1, message1)
-    // Read map
-    const getMessage = await getRedisObject(messageKey1)
-    console.log(getMessage)
-
-    // Store List
-    saveRedisList("messages", messages)
-    // Read List
-    const getMessages = await getRedisList("messages")
-    console.log(getMessages)
+const IdConstants = {
+    CHANNEL: "channel",
+    MESSAGE: "message",
+    MESSAGES: "messages",
+    USER: "user",
+    USERS: "users",
 }
 
-async function saveRedisObject(objectId, object) {
-    await client.hSet(objectId, serialize(object)); // hSet pour map
+function formatUniqueKey(prefix, objectId, suffix) {
+    if (suffix) {
+        return prefix + ":" + objectId + ":" + suffix
+    } else {
+        return prefix + objectId
+    }
+}
+
+function initDataSet() {
+    const users = [ //TODO rajouter liste message
+        { id: 'user1', name: 'Alice' },
+        { id: 'user2', name: 'Bob' },
+        { id: 'user3', name: 'Charlie' },
+        { id: 'user4', name: 'David' },
+        { id: 'user5', name: 'Eve' },
+    ];
+
+    const channels = [
+        { id: 'channel1', name: 'general' },
+        { id: 'channel2', name: 'random' },
+        { id: 'channel3', name: 'help' },
+    ];
+
+    const messages = [
+        {
+            id: 'message1',
+            author: users[0],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 10:00:00',
+            content: 'Hello everyone!'
+        },
+        {
+            id: 'message2',
+            author: users[1],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 10:05:00',
+            content: 'Hi Alice!'
+        },
+        {
+            id: 'message3',
+            author: users[2],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 10:10:00',
+            content: 'Happy New Year!'
+        },
+        {
+            id: 'message4',
+            author: users[3],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 10:15:00',
+            content: 'Can anyone help me with this issue?'
+        },
+        {
+            id: 'message5',
+            author: users[4],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 10:20:00',
+            content: 'Sure, what do you need help with?'
+        },
+        {
+            id: 'message6',
+            author: users[0],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 10:25:00',
+            content: 'I am also available to help.'
+        },
+        {
+            id: 'message7',
+            author: users[1],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 10:30:00',
+            content: 'Anyone up for a quick game?'
+        },
+        {
+            id: 'message8',
+            author: users[2],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 10:35:00',
+            content: 'I am!'
+        },
+        {
+            id: 'message9',
+            author: users[3],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 10:40:00',
+            content: 'Count me in.'
+        },
+        {
+            id: 'message10',
+            author: users[4],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 10:45:00',
+            content: 'Let\'s go!'
+        },
+        {
+            id: 'message11',
+            author: users[0],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 10:50:00',
+            content: 'What game are we playing?'
+        },
+        {
+            id: 'message12',
+            author: users[1],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 10:55:00',
+            content: 'How about a quick trivia?'
+        },
+        {
+            id: 'message13',
+            author: users[2],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 11:00:00',
+            content: 'I love trivia!'
+        },
+        {
+            id: 'message14',
+            author: users[3],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 11:05:00',
+            content: 'Trivia sounds fun.'
+        },
+        {
+            id: 'message15',
+            author: users[4],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 11:10:00',
+            content: 'Let\'s start!'
+        },
+        {
+            id: 'message16',
+            author: users[0],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 11:15:00',
+            content: 'First question: What is the capital of France?'
+        },
+        {
+            id: 'message17',
+            author: users[1],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 11:20:00',
+            content: 'Paris!'
+        },
+        {
+            id: 'message18',
+            author: users[2],
+            channel: channels[1],
+            attachments: [],
+            date: '01-01-2024 11:25:00',
+            content: 'Correct!'
+        },
+        {
+            id: 'message19',
+            author: users[3],
+            channel: channels[2],
+            attachments: [],
+            date: '01-01-2024 11:30:00',
+            content: 'Next question, please.'
+        },
+        {
+            id: 'message20',
+            author: users[4],
+            channel: channels[0],
+            attachments: [],
+            date: '01-01-2024 11:35:00',
+            content: 'What is 2 + 2?'
+        },
+    ];
+
+    for (const user of users) {
+        const key = formatUniqueKey(IdConstants.USER, user.id);
+        client.del(key)
+        saveRedisObject(key, user)
+
+        const keyMessages = formatUniqueKey(IdConstants.USER, user.id, IdConstants.MESSAGES);
+        client.del(keyMessages)
+    }
+
+    for (const channel of channels) {
+        const key = formatUniqueKey(IdConstants.CHANNEL, channel.id);
+        client.del(key)
+        saveRedisObject(key, channel)
+    }
+
+    for (const message of messages) {
+        const key = formatUniqueKey(IdConstants.MESSAGE, message.id);
+        client.del(key)
+        saveRedisObject(key, message)
+
+        const keyMessages = formatUniqueKey(IdConstants.USER, message.author.id, IdConstants.MESSAGES);
+        client.sAdd(keyMessages, key)
+    }
+}
+
+async function getUserMessages(userId) {
+    const messageKeys = await client.sMembers(formatUniqueKey(IdConstants.USER, userId, IdConstants.MESSAGES));
+    const messages = [];
+
+    for (const key of messageKeys) {
+        const message = await getRedisObject(key);
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function saveRedisObject(objectId, object) {
+    client.hSet(objectId, serialize(object));
 }
 
 async function getRedisObject(objectId) {
     const obj = await client.hGetAll(objectId);
     return deserialize(obj);
-}
-
-async function saveRedisList(listId, list) {
-    await client.rPush(listId, list.map(message => JSON.stringify(serialize(message)))); // rPush (=append) ou lPush (="insert(0)"") pour list
-}
-
-async function getRedisList(listId) {
-    const response = await client.lRange(listId, 0, -1);
-    return response.map(serializedMessage => deserialize(JSON.parse(serializedMessage)));
 }
 
 function serialize(obj) {
@@ -108,63 +275,15 @@ function deserialize(obj) {
     return deserializedObj;
 }
 
-// Map Keys will be like "user:idUser1", "user:idUser2", "channel:idUser1". Use formatUniqueKey() function like this: formatUniqueKey(MapKeyPrefix.MESSAGE, "idMessage123")
-// List Ids will be like "channels", "users". Just use ListKey.USERS
-const MapKeyPrefix = {
-    MESSAGE: "message:",
-    USER: "user:",
-    EVALUATION: "evaluation:",
-    CHANNEL: "channel:"
-}
+async function main() {
+    initDataSet()
 
-const ListKey = {
-    ROLES: "roles",
-    USERS: "users",
-    EVALUATIONS: "evaluations",
-    EVALUATION_CRITERIAS: "evaluation_criterias"
-}
+    const userId = "user3"
+    const messagesUser = await getUserMessages(userId)
+    const user = await getRedisObject(formatUniqueKey(IdConstants.USER, userId))
 
-function formatUniqueKey(prefix, objectId) {
-    return prefix + objectId
-}
-
-async function scanBdd() {
-    // Boucle infini, chatGPT m'a donné du caca mais affiche quand meme le contenu de la BDD
-    await client.connect();
-    let cursor = '0';
-    do {
-        const reply = await client.scan(cursor);
-        cursor = reply.cursor;
-        const keys = reply.keys;
-
-        for (const key of keys) {
-            const type = await client.type(key);
-            let value;
-            switch (type) {
-                case 'string':
-                    value = await client.get(key);
-                    break;
-                case 'hash':
-                    value = await client.hGetAll(key);
-                    break;
-                case 'list':
-                    value = await client.lRange(key, 0, -1);
-                    break;
-                case 'set':
-                    value = await client.sMembers(key);
-                    break;
-                case 'zset':
-                    value = await client.zRange(key, 0, -1);
-                    break;
-                default:
-                    value = 'Unsupported type';
-            }
-            console.log(`Key: ${key}, Type: ${type}, Value:`, value);
-        }
-    } while (cursor !== '0');
-
-    await client.quit();
+    console.log(user)
+    console.log(messagesUser)
 }
 
 main()
-// scanBdd()
