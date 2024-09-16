17e708c656faf54184e2498b354520aae6d8c632
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2024-09-16 14:57:04 +0200

    better ui form + fix startup script

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ last_modified_file=".last_modified"
 git_url=https://github.com/AmauryDelannoyPro/discord-bot-jb/archive/refs/heads/main.zip
 path_env=../.dev.env
 path_app=src/app/app.js
+repo_api_url="https://api.github.com/repos/AmauryDelannoyPro/discord-bot-jb/commits/main"
 
 # Fonction pour vérifier si une commande est disponible
 command_exists() {
@@ -29,9 +30,9 @@ fi
 
 # Vérification si une nouvelle version du ZIP est disponible
 echo "Vérification de la version du projet sur GitHub..."
-last_modified=$(curl -sI "$git_url" | grep -i "Last-Modified" | sed 's/Last-Modified: //I')
+last_commit=$(curl -s "$repo_api_url" | grep '"sha"' | head -n 1 | sed 's/.*"sha": "\(.*\)",/\1/')
 
-if [ -f "$last_modified_file" ] && [ "$(cat $last_modified_file)" == "$last_modified" ]; then
+if [ -f "$last_modified_file" ] && [ "$(cat $last_modified_file)" == "$last_commit" ]; then
     echo "Aucune nouvelle version disponible. Pas de téléchargement nécessaire."
 else
     echo "Nouvelle version disponible. Téléchargement en cours..."
@@ -46,7 +47,7 @@ else
     rm -f .project.zip
 
     # Enregistrer la date de la dernière modification
-    echo "$last_modified" > "$last_modified_file"
+    echo "$last_commit" > "$last_modified_file"
 fi
 
 cd $project_dir
